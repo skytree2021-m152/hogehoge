a290325120fa2b2c81ccb13d6b38df6a9a80e4f5
m115 <m115@skytree2021.site>
2021-06-14 15:27:41 +0900

    umekomi

--- web_image yamamchi.py
+++ web_image yamamchi.py
@@ -8,11 +8,12 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 tw = mytw.MyTwitter()
 idlist=[]
+umekomi=[]
 geturl = "https://api.twitter.com/1.1/statuses/oembed.json?id="
 
 if tw.api is not None:
   #リストの中から最新2ツイートを取得
-    for status in tw.api.list_timeline(list_id=1403224831550648321, count=2):
+    for status in tw.api.list_timeline(list_id=1403224831550648321, count=3, include_rts=1):
         
         #返ってきたtweetのidを取得
         tweet_id = status.id
@@ -33,11 +34,14 @@ def GetJMAData(request):
         return None
 
 if __name__ == '__main__':
-  #idlistの長さだけfor文
+  #idlistの長さだけfor文(3回予定)
   for i in range(len(idlist)):
     #リクエストURLにツイートIDつける
     request = geturl+str(idlist[i])
     res = GetJMAData(request)
-    #JSONデータ(res)のhtmlをプリント
-    print(res["html"])
+    #JSONデータ(res)のhtmlをumekomiリストに入れる
+    umekomi.append(res["html"])
+
+#umekomiを確認(いらなくなったら#つけてください)
+print(umekomi)
 
