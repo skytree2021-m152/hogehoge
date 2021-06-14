9f3b7557404770e097b43d8682f722e54eb727d9
m115 <m115@skytree2021.site>
2021-06-14 13:47:36 +0900

    リツイート成功

--- ツイッター自動投稿のときのやつ/trendretweet2.py
+++ ツイッター自動投稿のときのやつ/trendretweet2.py
@@ -8,11 +8,11 @@ tw = mytw.MyTwitter()
 
 if tw.api is not None:
   #リストの中から、今日、3個を検索
-    for status in tw.api.search(q="list:1403224831550648321 include:nativeretweets since:{}".format(datetime.date.today()), count=3):
+    for status in tw.api.list_timeline(list_id=1403224831550648321, count=2):
         
         #返ってきたtweetのidを取得
         tweet_id = status.id
-        print(status) 
+        print(tweet_id)
         # 例外処理をする
         try:
             # リツイート実行（for文の中なので繰り返す）
