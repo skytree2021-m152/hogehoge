f9848fa74e34634ee0f624f5e6d8b6853982cac3
m114 <m114@skytree2021.site>
2021-06-14 15:33:54 +0900

    aaa

---
+++ matsui.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://www3.nhk.or.jp/news/html/20210614/k10013083911000.html')
+soup = BeautifulSoup(response.text,'lxml')
+title = soup.title.string
+print(title)
