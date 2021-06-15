e28ab385828716a865359ccf6ec4b73d1f3c23be
m152 <m152@skytree2021.site>
2021-06-15 13:32:37 +0900

    変更しました

--- status_lookup.py
+++ status_lookup.py
@@ -4,13 +4,13 @@ import mytw, tweepy, traceback,datetime
 tw = mytw.MyTwitter()
 
 #欲しいツイートのIDをここに入れる
-id_ = "1404593501405257728"
+id_ = "1404559336010092546"
 
 if tw.api is not None:
-    statuses= tw.api.get_status(id_)
+    statuses= tw.api.get_status(id_,tweet_mode='extended')
     #print(status._json["entities"]["urls"][0]["url"])--エラーでる
     #返ってきたtweetのidを取得
-    print(statuses._json)
+    print(statuses.full_text)
 
 else:
     print(traceback.format_exc())
