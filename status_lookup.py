4caffa79984f4504fa676c4c3c0c6119233bde06
m152 <m152@skytree2021.site>
2021-06-15 13:42:54 +0900

    fulltext

--- status_lookup.py
+++ status_lookup.py
@@ -4,7 +4,7 @@ import mytw, tweepy, traceback,datetime
 tw = mytw.MyTwitter()
 
 #欲しいツイートのIDをここに入れる
-id_ = "1404559336010092546"
+id_ = "1404621357917237248"
 
 if tw.api is not None:
     #print(status._json["entities"]["urls"][0]["url"])--エラーでる
