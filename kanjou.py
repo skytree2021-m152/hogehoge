638b21494ae2ac0b78b325026b5c6198abb438f1
m115 <m115@skytree2021.site>
2021-06-15 16:39:34 +0900

    kanjoubunnseki

---
+++ kanjou.py
@@ -0,0 +1,19 @@
+#!/usr/share/nginx/.virtualenvs/env3.7/bin/python
+
+import io, sys, os, json, codecs, random, datetime, cgi, requests
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+
+def main(content, access_token="AIzaSyAzX7s-hgiekJ7KzSJvfI2yoElbEcZrpLM"):
+    geturl = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key={}'.format(access_token)
+    h = {'Content-Type': 'application/json'}
+    b = {
+        "document": {
+            "type": "PLAIN_TEXT",
+            "language": "JA",
+            "content": content
+        },
+        "encodingType": "UTF8"
+    }
+    res = requests.post(geturl, headers=h, json=b).json()
+    return res
