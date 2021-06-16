03fa5fae792b3d3cfb74b49ed2e531525cac09b7
m115 <m115@skytree2021.site>
2021-06-16 15:02:23 +0900

    a

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -4,29 +4,55 @@ import io, sys, os, codecs, random
 import mytw, tweepy, traceback,datetime
 import requests, datetime, json, os, sys, psycopg2, traceback, configparser
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+#pythonファイルのインポート
+#scraping.の後はリストに応じたファイル名を指定！
+import default as s,kanjou
 
 # InsecureRequestWarning対策
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-
 tw = mytw.MyTwitter()
+
+#ツイートIDのリスト
 idlist=[]
+#ツイートのステータス(JSON)のリスト
+statuses=[]
+#Webサイトに埋め込む用HTMLのリスト
 umekomi=[]
+#感情分析に入れたい文のリスト
+texts=[]
+#umekomiをもらうためのURL
 geturl = "https://api.twitter.com/1.1/statuses/oembed.json?id="
 
 if tw.api is not None:
   #リストの中から最新3ツイートを取得(リツイートを含む)
-    for status in tw.api.list_timeline(list_id=1403224831550648321, count=3, include_rts=1):
-        #print(status._json["entities"]["urls"][0]["url"])--エラーでる
-        #返ってきたtweetのidを取得
-        tweet_id = status.id
-        idlist.append(tweet_id)
+    for status in tw.api.list_timeline(list_id=1404673140899282948, count=3, include_rts=1):
+      #リツイートされたものか判定
+        if "retweeted_status" in status._json:
+          #print("リツイートされているツイート",status.id)
 
+          #リツイートされた元々のツイートIDを引っ張ってくる処理
+          tweet_id = status._json["retweeted_status"]["id"]
+          idlist.append(tweet_id)
+      
+        else:
+          #print("リツイートではないツイート",status.id)
+          #返ってきたtweetのidを取得
+          tweet_id = status.id
+          idlist.append(tweet_id)
+
+    for i in range(len(idlist)):
+      #statusではツイート本文が途中で切れてしまう問題を解決
+      stat=tw.api.get_status(idlist[i],tweet_mode='extended')
+      statuses.append(stat._json)
+      
 
 else:
     print(traceback.format_exc())
 
-print(idlist)
+#print(idlist)
 
 #GetJMADataの定義、JSONとってくる
 def GetJMAData(request):
@@ -43,13 +69,38 @@ if __name__ == '__main__':
     request = geturl+str(idlist[i])
     res = GetJMAData(request)
     #JSONデータ(res)のhtmlをumekomiリストに入れる
-    res2=res["html"]
-    umekomi.append(res2)
+    umekomi.append(res["html"])
 
-#umekomiを確認(いらなくなったら#つけてください)
+#umekomiを確認
 #print(umekomi)
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
+for i in range(len(statuses)):
+
+  #urlのあるツイートか判定
+  if statuses[i]["entities"]["urls"]:
+    #ツイートに載っているリンクURLを変数に代入
+    url=statuses[i]["entities"]["urls"][0]["url"]
+    #urlを別ファイルに渡して実行
+    text = s.func(url)
+    #print(text)
+    texts.append(text)
+
+  else:
+    #ツイートの本文取得
+    #print(statuses[i]["full_text"])
+    texts.append(statuses[i]["full_text"])
+
+mag=[]
+score=[]
+#３つのツイートを感情分析APIに入れる
+for i in range(3):
+  res = kanjou.main(texts[i])
+  #CGIの時は↓はけすこと 
+  mag.append(res['documentSentiment']["magnitude"])
+  score.append(res['documentSentiment']['score'])
+
+#print(mag)
+#print(score)
 title_str = 'TWITTER トレンド'
 
 print('''
@@ -81,7 +132,7 @@ Content-type: text/html
 </div>
 <div class="chatting">
 <div class="says">
-<h2>感情のデカさ：ほげ1<br>いい話度：hoge1</h2></div>
+<h2>感情のデカさ：{mag1}<br>いい話度：{sc1}</h2></div>
 </div>
 </div>
 </div>
@@ -94,7 +145,7 @@ Content-type: text/html
 </div>
 <div class="chatting">
 <div class="says">
-<h2>感情のデカさ：ほげ2<br>いい話度：hoge2</h2></div>
+<h2>感情のデカさ：{mag2}<br>いい話度：{sc2}</h2></div>
 </div>
 </div>
 </div>
@@ -107,7 +158,7 @@ Content-type: text/html
 </div>
 <div class="chatting">
 <div class="says">
-<h2>感情のデカさ：ほげ3<br>いい話度：hoge3</h2></div>
+<h2>感情のデカさ：{mag3}<br>いい話度：{sc3}</h2></div>
 </div>
 </div>
 </div>
@@ -127,4 +178,4 @@ Content-type: text/html
 
 </body>
 </html>
-'''[1:-1].format(title=title_str,tw1=umekomi[0],tw2=umekomi[1],tw3=umekomi[2]))
+'''[1:-1].format(title=title_str,tw1=umekomi[0],tw2=umekomi[1],tw3=umekomi[2],mag1=mag[0],mag2=mag[1],mag3=mag[2],sc1=score[0],sc2=score[1],sc3=score[2]))
